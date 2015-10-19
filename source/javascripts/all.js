8fd304fa670de09cb0b34904e6d1f70d8a80a199
jaclynperrone <jaclyn.perrone@gmail.com>
2015-10-19 11:56:15 -0400

    Made side navigation fixed.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,18 @@
 //= require jquery  
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function(){
+	var navigation = $('.navigation-ul');
+	var navigationTop = navigation.offset().top;
+
+	$(window).scroll(function() {
+	  var scroll = $(window).scrollTop();
+	  console.log('scroll='+scroll+'top='+navigationTop);
+	  if (scroll >= navigationTop) {
+	    navigation.addClass('fixed-navigation');
+	  } else {
+	    navigation.removeClass('fixed-navigation');
+	  }
+	});
+});
